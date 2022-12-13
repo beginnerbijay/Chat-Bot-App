6d59c7cd8f19319e07d319b9bff5e64d952c01fe
D Bijayshakti Patro <dbijaypatro@gmail.com>
2022-12-13 13:20:03 +0530

    updated

--- public/script.js
+++ public/script.js
@@ -1,37 +1,33 @@
 const inputValue = document.getElementById("input");
+const btn = document.getElementById("btn");
+
 const messagesContainer = document.getElementById("messages");
 document.addEventListener("DOMContentLoaded", () => {
   inputValue.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
-      let input = inputValue.value;
-      inputValue.value = "";
-      let userDiv = document.createElement("div");
-      let userText = document.createElement("span");
-      userDiv.id = "user";
-      userDiv.className = "user response";
-      userText.innerHTML = `${input}`;
-      userDiv.appendChild(userText);
-      messagesContainer.appendChild(userDiv);
-      addChat(input);
+      addChatData()
     }
   });
+  btn.addEventListener("click",()=>{
+    addChatData()
+  })
 });
+
+function addChatData(){
+  let input = inputValue.value;
+  inputValue.value = "";
+  let userDiv = document.createElement("div");
+  let userText = document.createElement("span");
+  userDiv.id = "user";
+  userDiv.className = "user response";
+  userText.innerHTML = `${input}`;
+  userDiv.appendChild(userText);
+  messagesContainer.appendChild(userDiv);
+  addChat(input);
+}
 
 async function addChat(prompt) {
   try {
-    const { data } = await axios.post("/", { prompt });
-    if (data) {
-      const botres = data.split("\n").join("");
-      output(botres);
-    } else {
-      alert("something went worng");
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-function output(product) {
     let botDiv = document.createElement("div");
     let botText = document.createElement("span");
     botDiv.id = "bot";
@@ -39,5 +35,14 @@ function output(product) {
     botText.innerText = "Typing...";
     botDiv.appendChild(botText);
     messagesContainer.appendChild(botDiv);
-  botText.innerText = `${product}`;
+    const { data } = await axios.post("/", { prompt });
+    if (data) {
+      const botres = data.split("\n").join("");
+      botText.innerText = `${botres}`;
+    } else {
+      alert("something went worng");
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
